61d72bc4f2ad2e59800b5a9e66a12c3820372247
Jeff Klenzing <jklenzing@gmail.com>
2016-11-17 13:58:42 -0500

    Code Cleanup
    Removed line integrals, renamed several functions

--- solarind.py
+++ solarind.py
@@ -32,31 +32,28 @@ class index:
 
 		
 		S = Dataset(file, 'r')
-		self.year = np.floor(S.variables['DATE'][0,:]/1000)
-		self.day  = np.mod(S.variables['DATE'][0,:],1000)
+		self.year  = np.floor(S.variables['DATE'][0,:]/1000)
+		self.day   = np.mod(S.variables['DATE'][0,:],1000)
 		self.fyear = self.year + self.day/367.0
-		self.dn  = np.array([datetime.datetime(int(self.year[i]), 1, 1, 12, 0) + datetime.timedelta(days=int(self.day[i]-1)) for i in range(0,len(self.year))])
+		self.dn    = np.array([datetime.datetime(int(self.year[i]), 1, 1, 12, 0) + datetime.timedelta(days=int(self.day[i]-1)) for i in range(0,len(self.year))])
 
-		self.cor_1au = fixnan(S.variables['COR_1AU'][0,:])
-		self.He2 = fixnan(S.variables['LINE_FLUX'][0,:,1])
+		self.cor_1au   = fix_nan(S.variables['COR_1AU'][0,:])
+		self.He2       = fix_nan(S.variables['LINE_FLUX'][0,:,1])
 		
-		self.sp_wave   = fixnan(S.variables['SP_WAVE'][0,:])
-		self.sp_flux   = fixnan(S.variables['SP_FLUX'][0,:,:])
-		self.line_wave = fixnan(S.variables['LINEWAVE'][0,:])
-		self.line_flux = fixnan(S.variables['LINE_FLUX'][0,:,:])
+		self.sp_wave   = fix_nan(S.variables['SP_WAVE'][0,:])
+		self.sp_flux   = fix_nan(S.variables['SP_FLUX'][0,:,:])
+		self.line_wave = fix_nan(S.variables['LINEWAVE'][0,:])
+		self.line_flux = fix_nan(S.variables['LINE_FLUX'][0,:,:])
 
-		bins, area, lines, larea= coeff_o()
+		bins, area = coeff()
 
 		self.oxygen = np.zeros(len(self.year))
 		for i in range(0,len(area)):
-			self.oxygen = self.oxygen + sp_int(bins[:,i],self.sp_wave,self.sp_flux,area[i])
+			self.oxygen = self.oxygen + integrate(bins[:,i],self.sp_wave,self.sp_flux,area[i])
 
-		self.loxygen = np.zeros(len(self.year))
-		for i in range(0,len(larea)):
-			self.loxygen = self.loxygen + line_int(lines[i],self.sp_wave,self.sp_flux,area[i])
+#-- End of index
 
-
-def sp_int(b,x,y,s):
+def integrate(b,x,y,s):
 	'''
 	Integrates sp_flux over bin values 
 
@@ -69,27 +66,24 @@ def sp_int(b,x,y,s):
 	Ouput:
 	iflux = Integrated flux for bin values
 	'''
+	d_lambda = 1.0 # nm
 	ind = (x >= b[0]) & (x < b[1])
-	iflux = s*np.sum(y[:,ind],axis=1)
+	iflux = s*np.sum(y[:,ind],axis=1)*d_lambda
 	return(iflux)
 
-def line_int(line,x,y,s):
-	'''
-	Integrates line_flux over bin values 
-	'''
-	ind = np.abs(line-x)<0.1
-	iflux = s*np.sum(y[:,ind],axis=1)
-	return(iflux)
+#-- End of integrate
 
-def fixnan(x):
+def fix_nan(x):
 	'''
 	Replaces missing values (-1) with nan
 	'''
-	x[x==-1] = np.nan
+	x[x<0] = np.nan
 
 	return x
 	
-def coeff_o():
+#-- End of fix_nan
+
+def coeff(species='o'):
 	'''
 	Generates bins of O ionization coefficients using method described by Richards et al, 1994.  Bin widths for lines taken from EUVAC code.  
 	Cross-sectional areas for lines taken as the difference between the published value and the wide bin in which the line resides.
@@ -109,11 +103,14 @@ def coeff_o():
          				45.   ,   46.822,   50.   ,   55.   ,   55.731,   58.733,   60.   ,   61.276,   63.273,   65.   ,   70.   ,   70.636,
          				75.   ,   76.815,   77.341,   79.236,   80.   ,   85.   ,	90.   ,   95.   ,   98.002,  100.   ,  102.872,  103.491]])
 	
-	area = np.array([	 0.73 ,	   1.839,    3.732,    5.202,   -0.411,    0.619,    6.461,   -1.013,   -0.993,    8.693,    0.153,    9.687,   
-						11.496,   -0.197,	12.127,   12.059,   -0.434,	   0.066,   13.024,    0.035,    0.035,   13.365,   17.245,    0.724,
-						10.736,	  -1.091,	-1.201,   -1.342,    5.091,    3.498,	 4.554,	   1.315,	 0.0  ,	   0.0  ,    0.0  ,    0.0  ])*1e-22 # m^2
+	if species == 'o':
+		area = np.array([ 0.73 ,   1.839,    3.732,    5.202,   -0.411,    0.619,    6.461,   -1.013,   -0.993,    8.693,    0.153,    9.687,   
+						 11.496,  -0.197,	12.127,   12.059,   -0.434,	   0.066,   13.024,    0.035,    0.035,   13.365,   17.245,    0.724,
+						 10.736,  -1.091,	-1.201,   -1.342,    5.091,    3.498,	 4.554,	   1.315,	 0.0  ,	   0.0  ,    0.0  ,    0.0  ])*1e-22 # m^2
+	else:
+		raise Exception('Invalid species')
 
-	lines =  np.array([25.63, 28.415, 30.331, 30.378, 36.807, 46.522, 55.437, 58.433, 60.976, 62.973, 70.336, 76.515, 77.041, 78.936])
-	larea =  np.array([6.05,7.08,7.68,7.7,9.8400,11.930,12.590,13.090,13.4,13.4,11.46,4,3.89,3.749])
-	
-	return bins, area, lines, larea
+	return bins, area
+
+#-- End of coeff_o
+
