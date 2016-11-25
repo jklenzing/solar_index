c8091cd1537ce8d666ef7947dfbe079df7e6fd9d
Jeff Klenzing <jklenzing@gmail.com>
2016-11-25 13:12:11 -0500

    Summary Figure
    added summary fig of oxygen power vs F10.7.  Also updated with yearly correlation coefficients

--- test.py
+++ test.py
@@ -21,6 +21,23 @@ S.Rz = f(S.fyear)
 f = sint.interp1d(ofyear, oF107, kind='nearest', bounds_error=False, fill_value=np.nan)
 S.F107 = f(S.fyear)
 
-ind = np.where(~np.isnan(S.F107) & ~np.isnan(S.oxygen))[0]
+ind = ~np.isnan(S.F107) & ~np.isnan(S.oxygen)
 
-print 'He2 & F10.7 => %4.2f' % np.corrcoef(S.oxygen[ind],S.F107[ind])[0,1]
+print 'Oxygen Cross Section & F10.7 => %4.2f' % np.corrcoef(S.oxygen[ind],S.F107[ind])[0,1]
+
+for year in range(2003,2017):
+	jnd = np.logical_and(ind, S.year==year)
+	print '%d => %4.2f' % (year, np.corrcoef(S.oxygen[jnd],S.F107[jnd])[0,1])
+
+p = np.polyfit(S.F107[ind],S.oxygen[ind]*1e24,2)
+x = range(70,201)
+y = np.polyval(p,x)
+
+plt.plot(S.F107[ind],S.oxygen[ind]*1e24,'.k')
+plt.hold(True)
+plt.plot(x,y,'r',linewidth=2)
+plt.xlabel('F10.7 (sfu)', fontsize=16)
+plt.ylabel('Oxygen Power (yW)', fontsize=16)
+plt.title('2002 - 2015')
+plt.show()
+
