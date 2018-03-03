887bad62e73043339946e493360e766ef59527ca
jklenzing <jklenzing@gmail.com>
2018-03-02 19:49:04 -0500

    Fixed datetime

--- solar_index/solar_index.py
+++ solar_index/solar_index.py
@@ -114,24 +114,24 @@ class SolarIndex:
 
         # Construct filename and load the data
         assert path.isdir(file_dir), logging.error("unknown file directory")
-        self.filename = "%s/%s".format(file_dir, file_name)
+        self.filename = ('%s/%s' % (file_dir, file_name))
 
         assert path.isfile(self.filename), logging.error("unknown file")
 
         try:
-        S = Dataset(self.filename, 'r')
+            S = Dataset(self.filename, 'r')
         except:
             logging.error("unable to load netCDF4 file")
 
         # Assign the time data
         self.year = np.floor(S.variables['DATE'][0,:] / 1000.0).astype(int)
-        max_day = np.array([float(dt.datetime(yy, 12, 31).strftime("%j"))
-                            for yy in year])
+        #max_day = np.array([float(dt.datetime(yy, 12, 31).strftime("%j"))
+        #                    for yy in year])
         self.day = np.mod(S.variables['DATE'][0,:], 1000).astype(int)
-        self.fyear = self.year + self.day / (max_day + 1.0)
-        self.dn = np.array([dt.datetime(yy, 1, 1, 12)
-                            + dt.timedelta(days=int(self.day[i])-1)
-                            for i,yy in enumerate(iyear)])
+        #self.fyear = self.year + self.day / (max_day + 1.0)
+        self.dn =  np.array([dt.datetime(int(self.year[i]),1,1) +
+                            dt.timedelta(days=int(self.day[i])-1)
+                            for i in range(len(self.day))])
 
         self.cor_1au = self._fix_nan(S.variables['COR_1AU'][0,:])
         self.He2 = self._fix_nan(S.variables['LINE_FLUX'][0,:,1])
@@ -142,20 +142,20 @@ class SolarIndex:
         self.line_flux = self._fix_nan(S.variables['LINE_FLUX'][0,:,:])
 
     def integrate_power(self, species):
-    """ Integrates EUV spectra times photoionization cross-section
+        """ Integrates EUV spectra times photoionization cross-section
 
-    Parameters
-    ----------
-    species : (string)
-        Specifies which species to integrate for.
-        Currently supports 'o', 'n2', 'o2'
+        Parameters
+        ----------
+        species : (string)
+            Specifies which species to integrate for.
+            Currently supports 'o', 'n2', 'o2'
 
-    Returns
-    -------
-    self.power : (dict)
-        Dictionary containing the average power delivered to a given ion as
-        a timeseries.
-    """
+        Returns
+        -------
+        self.power : (dict)
+            Dictionary containing the average power delivered to a given ion as
+            a timeseries.
+        """
         assert species in self.species, logging.error("unknown species")
 
         bins, area = self.load_coeff(species=species)
@@ -169,90 +169,90 @@ class SolarIndex:
     def _integrate_bin(b,x,y,s):
         """ Integrates sp_flux over bin values
 
-    Parameters
-    ----------
-    b : (float)
-        bounds of bins (b0, b1)
-    x : (float)
-        wavelengths
-    y : (float)
-        flux spectra
-    s : (float)
-        ionization cross-section for bin of interest
+        Parameters
+        ----------
+        b : (float)
+            bounds of bins (b0, b1)
+        x : (float)
+            wavelengths
+        y : (float)
+            flux spectra
+        s : (float)
+            ionization cross-section for bin of interest
 
-    Returns
-    -------
-    iflux : (float)
-        Integrated flux for bin
-    """
-    d_lambda = 1.0 # nm
-    ind = (x >= b[0]) & (x < b[1])
-    iflux = s * np.sum(y[:,ind], axis=1) * d_lambda
-    return(iflux)
+        Returns
+        -------
+        iflux : (float)
+            Integrated flux for bin
+        """
+        d_lambda = 1.0 # nm
+        ind = (x >= b[0]) & (x < b[1])
+        iflux = s * np.sum(y[:,ind], axis=1) * d_lambda
+        return(iflux)
 
     def _fix_nan(x, fill_value=-1, replace_value=np.nan):
         """ Replaces missing values (-1) with nan
 
-    Parameters
-    ----------
-    x : (np.array)
-        Array of values with some values possibly filled by a constant
-        fill_value : (float)
-            Value used to denote a lack of data (default=-1)
-        replace_value : (float)
-            New fill value (default=np.nan)
+        Parameters
+        ----------
+        x : (np.array)
+            Array of values with some values possibly filled by a constant
+            fill_value : (float)
+                Value used to denote a lack of data (default=-1)
+            replace_value : (float)
+                New fill value (default=np.nan)
 
-    Returns
-    -------
-    x : (np.array)
-            Array of values with old fill values replaced with new fill values
-    """
+        Returns
+        -------
+        x : (np.array)
+                Array of values with old fill values replaced with new fill values
+        """
         assert isinstance(x, np.ndarray), \
                 logging.error("x must be a numpy array")
 
         x[x==fill_value] = replace_value
 
-    return x
+        return x
 
 
     def load_coeff(species):
         """ Generates bins of photoabsorption coefficients using method
         described by Solomon et al, 2005.
 
-    Parameters
-    ----------
-    species : (string)
-            String denoting coefficients to load ('o', 'o2', 'n2' supported)
+        Parameters
+        ----------
+        species : (string)
+                String denoting coefficients to load ('o', 'o2', 'n2' supported)
 
-    Returns
-    -------
-    bins : (float)
-        coordinates of min and max of each bin in nm
-    area : (float)
-        The corresponding ionization cross-section (m^2)
-    """
+        Returns
+        -------
+        bins : (float)
+            coordinates of min and max of each bin in nm
+        area : (float)
+            The corresponding ionization cross-section (m^2)
+        """
         assert species in self.species, logging.error("unknown species")
 
-    bins = np.array([[0.05, 0.4, 0.8, 1.8, 3.2, 7.0, 15.5, 22.4, 29.0, 32.0,
-      54.0, 65.0, 79.8, 91.3, 97.5, 98.7, 102.7],
-             [0.4, 0.8, 1.8, 3.2, 7.0, 15.5, 22.4, 29.0, 32.0, 54.0,
-      65.0, 79.8, 91.3, 97.5, 98.7, 102.7, 105.0]])
+        bins = np.array([[0.05, 0.4, 0.8, 1.8, 3.2, 7.0, 15.5, 22.4, 29.0, 32.0,
+          54.0, 65.0, 79.8, 91.3, 97.5, 98.7, 102.7],
+                 [0.4, 0.8, 1.8, 3.2, 7.0, 15.5, 22.4, 29.0, 32.0, 54.0,
+          65.0, 79.8, 91.3, 97.5, 98.7, 102.7, 105.0]])
 
-    # Currently using lowest of split bins, units of square meters
-    if species == 'o':
-        area = np.array([0.0023, 0.0170, 0.1125, 0.1050, 0.3247, 1.3190,
-                             3.7832, 6.0239, 7.7205, 10.7175, 13.1253, 8.5159,
-                             3.0031, 0.0000, 0.0000, 0.0000, 0.0000]) * 1.0e-22
-    elif species == 'o2':
-        area = np.array([0.0045, 0.0340, 0.2251, 0.2101, 0.6460, 2.6319,
-                             7.6283, 13.2125, 16.8233, 20.3066, 27.0314,
-                             23.5669, 10.4980, 13.3950, 18.7145, 1.6320,
-                             1.1500]) * 1.0e-22
-    elif species == 'n2':
-        area = np.array([0.0025, 0.0201, 0.1409, 1.1370, 0.3459, 1.5273,
-                             5.0859, 9.9375, 11.7383, 19.6514, 23.0931, 23.0346,
-                             2.1434, 2.1775, 2.5465, 0.0000, 0.0000]) * 1.0e-22
-    else:
-        raise Exception('Invalid species')
+        # Currently using lowest of split bins, units of square meters
+        if species == 'o':
+            area = np.array([0.0023, 0.0170, 0.1125, 0.1050, 0.3247, 1.3190,
+                                 3.7832, 6.0239, 7.7205, 10.7175, 13.1253, 8.5159,
+                                 3.0031, 0.0000, 0.0000, 0.0000, 0.0000]) * 1.0e-22
+        elif species == 'o2':
+            area = np.array([0.0045, 0.0340, 0.2251, 0.2101, 0.6460, 2.6319,
+                                 7.6283, 13.2125, 16.8233, 20.3066, 27.0314,
+                                 23.5669, 10.4980, 13.3950, 18.7145, 1.6320,
+                                 1.1500]) * 1.0e-22
+        elif species == 'n2':
+            area = np.array([0.0025, 0.0201, 0.1409, 1.1370, 0.3459, 1.5273,
+                                 5.0859, 9.9375, 11.7383, 19.6514, 23.0931, 23.0346,
+                                 2.1434, 2.1775, 2.5465, 0.0000, 0.0000]) * 1.0e-22
+        else:
+            raise Exception('Invalid species')
 
-    return bins, area
+        return bins, area
