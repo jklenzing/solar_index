b221138ee8c2446c3a723444075da8981c3c75fb
jklenzing <jklenzing@gmail.com>
2018-03-20 11:46:38 -0400

    Improved pandas support

--- sw_paper.py
+++ sw_paper.py
@@ -2,28 +2,38 @@ import solar_index
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
+import scipy
 
 S = solar_index.EUVspectra()
 F = solar_index.OMNIvals()
 
-ind = ~np.isnan(S.power['o'])
-Opow = pd.DataFrame(S.power['o'][ind], index=S.dt[ind])
+ind1 = ~np.isnan(S.power['o'])
+Opow = pd.Series(S.power['o'][ind1], index=S.dt[ind1])
 
-ind = ~np.isnan(F.F107)
-F107 = pd.DataFrame(F.F107[ind], index=F.dt[ind])
+ind2 = ~np.isnan(F.F107)
+F107 = pd.Series(F.F107[ind2], index=F.dt[ind2])
+
+dt = np.intersect1d(S.dt[ind1],F.dt[ind2])
+df = pd.DataFrame({'F10.7':F107[dt], 'Opow':Opow[dt]})
 
 window = 81
 
-Opow_mean = Opow.rolling(window=window,center=True).mean()
-Opow_std = Opow.rolling(window=window,center=True).std()
-Opow_nrm = (Opow-Opow_mean)/Opow_std
+Opow_mean = df['Opow'].rolling(window=window,center=True).mean()
+Opow_std = df['Opow'].rolling(window=window,center=True).std()
+Opow_nrm = (df['Opow']-Opow_mean)/Opow_std
 
-F107_mean = F107.rolling(window=window,center=True).mean()
-F107_std = F107.rolling(window=window,center=True).std()
-F107_nrm = (F107-F107_mean)/F107_std
+F107_mean = df['F10.7'].rolling(window=window,center=True).mean()
+F107_std = df['F10.7'].rolling(window=window,center=True).std()
+F107_nrm = (df['F10.7']-F107_mean)/F107_std
+
+ind = ~np.isnan(F107_nrm)
+r,p = scipy.stats.pearsonr(F107_nrm[ind],Opow_nrm[ind])
+
+print(r)
+print(p)
 
 plt.plot(F107_nrm, label='F10.7')
-plt.plot(Opow_nrm, label='O power')
+plt.plot(Opow_nrm, label='O power', color='r')
 plt.legend()
 plt.show()
