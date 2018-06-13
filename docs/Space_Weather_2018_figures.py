1250c1e4aa698b0a80a2660a1906919850dec088
jklenzing <jklenzing@gmail.com>
2018-06-13 12:03:44 -0400

    Added structure of paper

--- docs/Space_Weather_2018_figures.py
+++ docs/Space_Weather_2018_figures.py
@@ -16,6 +16,8 @@ plt.ylabel('Photoabsorption Cross-section [m$^2$]')
 plt.savefig('figure1.png')
 plt.close()
 
+#### Normalization of datasets
+
 ind1 = ~np.isnan(S.power['o'])
 Opow = pd.Series(S.power['o'][ind1], index=S.dt[ind1])
 
@@ -36,13 +38,7 @@ F107_p = (df['F10.7']+F107_mean)/2.0
 F107_std = df['F10.7'].rolling(window=window,center=True).std()
 F107_nrm = (df['F10.7']-F107_mean)/F107_std
 
-r,p = scipy.stats.pearsonr(df['F10.7'],df['Opow'])
-ind = ~np.isnan(F107_nrm)
-rm,pm = scipy.stats.pearsonr(F107_mean[ind],Opow_mean[ind])
-rn,pn = scipy.stats.pearsonr(F107_nrm[ind],Opow_nrm[ind])
-
-print([r,rm,rn])
-print([p,pm,pn])
+#### Figure 2
 
 plt.ion()
 f, axarr = plt.subplots(2, sharex=True)
@@ -53,3 +49,18 @@ axarr[1].legend()
 axarr[0].plot(df['F10.7'], label='F10.7', color = 'k')
 axarr[0].plot(Opow_nrm*F107_std+F107_mean, label='O power', color='r')
 axarr[0].legend()
+
+#### Wavelet analysis of F10.7 and EUV data
+
+#### Figure 3
+
+#### Correlation coefficients of datasets
+r,p = scipy.stats.pearsonr(df['F10.7'],df['Opow'])
+ind = ~np.isnan(F107_nrm)
+rm,pm = scipy.stats.pearsonr(F107_mean[ind],Opow_mean[ind])
+rn,pn = scipy.stats.pearsonr(F107_nrm[ind],Opow_nrm[ind])
+
+print([r,rm,rn])
+print([p,pm,pn])
+
+#### Time Lag Analysis
