3451c6b4662d670024236c3e9c395e64c78682a9
Jeff Klenzing <jklenzing@gmail.com>
2019-01-29 18:39:45 -0500

    added euv tests for file path

--- solar_index/tests/test_euv.py
+++ solar_index/tests/test_euv.py
@@ -23,5 +23,12 @@ class TestEUV():
         """Runs after every method to clean up previous testing."""
         del self.testEUV
 
-    def test(self):
-        assert (True)
+    @raises(Exception)
+    def test_euv_load_w_bad_directory(self):
+        """Tests """
+        testEUV = EUVspectra(file_dir='bad_data')
+
+    @raises(Exception)
+    def test_euv_load_w_bad_file_name(self):
+        """Tests """
+        testEUV = EUVspectra(file_name='bad_data.ncdf')
